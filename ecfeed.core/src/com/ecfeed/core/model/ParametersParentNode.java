8c945156984ad0a61e91536f0a958921e0e4331e
Marek Kuczabinski <m.kuczabinski@radytek.com>
2017-03-15 12:56:34 +0100

    Code refactoring.

--- ecfeed.core/src/com/ecfeed/core/model/ParametersParentNode.java
+++ ecfeed.core/src/com/ecfeed/core/model/ParametersParentNode.java
@@ -20,34 +20,26 @@ public abstract class ParametersParentNode extends AbstractNode {
 	private List<AbstractParameterNode> fParameters;
 
 	public ParametersParentNode(String name) {
+
 		super(name);
 		fParameters = new ArrayList<AbstractParameterNode>();
 	}
 
-	public void addParameter(AbstractParameterNode parameter){
+	public abstract List<MethodNode> getMethods(AbstractParameterNode parameter);	
+
+	public void addParameter(AbstractParameterNode parameter) {
+
 		addParameter(parameter, fParameters.size());
 	}
 
 	public void addParameter(AbstractParameterNode parameter, int index) {
 
-		if (parameterWithNameExists(parameter)) {
+		if (parameterExists(parameter)) {
 			ExceptionHelper.reportRuntimeException("Parameter: " + parameter.getName() + " already exists.");
 		}
 
 		fParameters.add(index, parameter);
 		parameter.setParent(this);
-	}
-
-	private boolean parameterWithNameExists(AbstractParameterNode abstractParameterNode) {
-
-		for (AbstractParameterNode abstractParam : fParameters) {
-
-			if (abstractParam.getName().equals(abstractParameterNode.getName())) {
-				return true;
-			}
-		}
-
-		return false;
 	}
 
 	public List<AbstractParameterNode> getParameters(){
@@ -59,8 +51,9 @@ public abstract class ParametersParentNode extends AbstractNode {
 	}	
 
 	public AbstractParameterNode getParameter(String parameterName) {
-		for(AbstractParameterNode parameter : fParameters){
-			if(parameter.getName().equals(parameterName)){
+
+		for (AbstractParameterNode parameter : fParameters) {
+			if (parameter.getName().equals(parameterName)) {
 				return parameter;
 			}
 		}
@@ -76,56 +69,78 @@ public abstract class ParametersParentNode extends AbstractNode {
 		return true;
 	}
 
+	public boolean parameterExists(AbstractParameterNode abstractParameterNode) {
+
+		if (parameterExists(abstractParameterNode.getName())) {
+			return true;
+		}
+
+		return false;
+	}
+
 	public List<String> getParameterTypes() {
+
 		List<String> types = new ArrayList<String>();
+
 		for(AbstractParameterNode parameter : fParameters){
 			types.add(parameter.getType());
 		}
+
 		return types;
 	}
 
 	public List<String> getParametersNames() {
+
 		List<String> names = new ArrayList<String>();
+
 		for(AbstractParameterNode parameter : fParameters){
 			names.add(parameter.getName());
 		}
+
 		return names;
 	}
 
-	public boolean removeParameter(AbstractParameterNode parameter){
+	public boolean removeParameter(AbstractParameterNode parameter) {
+
 		parameter.setParent(null);
+
 		return fParameters.remove(parameter);
 	}
 
 	public void replaceParameters(List<AbstractParameterNode> parameters) {
+
 		fParameters.clear();
 		fParameters.addAll(parameters);
 	}
 
 	@Override
-	public List<? extends AbstractNode> getChildren(){
+	public List<? extends AbstractNode> getChildren() {
+
 		return fParameters;
 	}
 
 	@Override
-	public boolean isMatch(AbstractNode node){
-		if(node instanceof ParametersParentNode == false){
+	public boolean isMatch(AbstractNode node) {
+
+		if (node instanceof ParametersParentNode == false) {
 			return false;
 		}
+
 		ParametersParentNode comparedParent = (ParametersParentNode)node;
 
-		if(getParameters().size() != comparedParent.getParameters().size()){
+		if(getParameters().size() != comparedParent.getParameters().size()) {
 			return false;
 		}
-		for(int i = 0; i < getParameters().size(); ++i){
-			if(getParameters().get(i).isMatch(comparedParent.getParameters().get(i)) == false){
+
+		for (int i = 0; i < getParameters().size(); ++i) {
+
+			if (getParameters().get(i).isMatch(comparedParent.getParameters().get(i)) == false) {
 				return false;
 			}
 		}
+
 		return super.isMatch(node);
 	}
-
-	public abstract List<MethodNode> getMethods(AbstractParameterNode parameter);
 
 	public static String generateNewParameterName(ParametersParentNode fParametersParentNode, String startParameterName) {
 
