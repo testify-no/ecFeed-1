b6e25162e7c36cb48614e7bd3561e45cc76990fc
Marek Kuczabinski <m.kuczabinski@radytek.com>
2017-05-09 12:57:41 +0200

    Check for updates added to main menu.

---
+++ ecfeed.rcp3/src/com/ecfeed/rcp3/handlers/CheckForUpdatesRcpHandler.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.rcp3.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import com.ecfeed.ui.handlers.CheckForUpdatesHandler;
+
+
+public class CheckForUpdatesRcpHandler extends org.eclipse.core.commands.AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		CheckForUpdatesHandler.execute();
+		return null;
+	}
+}
