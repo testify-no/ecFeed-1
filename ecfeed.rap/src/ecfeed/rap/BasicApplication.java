0551011b1edfef91ae93b3d1f8f3a2840b69718b
Marek Kuczabinski <m.kuczabinski@radytek.com>
2017-06-28 13:40:44 +0200

    Initial RAP application.

---
+++ ecfeed.rap/src/ecfeed/rap/BasicApplication.java
@@ -0,0 +1,19 @@
+package ecfeed.rap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.rap.rwt.application.Application;
+import org.eclipse.rap.rwt.application.ApplicationConfiguration;
+import org.eclipse.rap.rwt.client.WebClient;
+
+
+public class BasicApplication implements ApplicationConfiguration {
+
+    public void configure(Application application) {
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put(WebClient.PAGE_TITLE, "ecFeed");
+        application.addEntryPoint("/hello", BasicEntryPoint.class, properties);
+    }
+
+}
