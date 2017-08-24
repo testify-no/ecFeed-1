fc94a8fea0d094e244c9992f986dce87be45cf07
hagertestify <hager.debech@testify.no>
2017-08-24 16:02:39 +0200

    added Next Page button and Previous page button

--- ecfeed/src/com/ecfeed/ui/dialogs/CulpritAnalysisDialog.java
+++ ecfeed/src/com/ecfeed/ui/dialogs/CulpritAnalysisDialog.java
@@ -18,7 +18,11 @@ import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -31,6 +35,7 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
 
 import com.ecfeed.core.generators.Culprit;
 import com.ecfeed.core.generators.TestResultDescription;
@@ -45,6 +50,10 @@ import com.ecfeed.ui.dialogs.basic.DialogObjectToolkit;
 
 public class CulpritAnalysisDialog extends TitleAreaDialog{
 	private DialogObjectToolkit fDialogObjectToolkit;
+	private int numRow = 0;
+	Table table = null;
+	private List<TestResultDescription> testResultDescrs = createtestResultDescrs();
+	private TestResultsAnalysis testResultsAnalysis = new TestResultsAnalyzer().generateAnalysis(testResultDescrs, 0, 3);
 
 	public CulpritAnalysisDialog(Shell parent) 
 	{
@@ -84,14 +93,16 @@ public class CulpritAnalysisDialog extends TitleAreaDialog{
 		createCombo(ChildComp2, 0);
 		createLabel(ChildComp2, "max");
 		createCombo(ChildComp2, 2);
-		Composite ChildComp3 = (Composite) fDialogObjectToolkit.createGridComposite(dialogArea, 1);
-		createTableContents(ChildComp3, 4);
+		Composite ChildComp3 = (Composite) fDialogObjectToolkit.createGridComposite(dialogArea, 2);
+		table = createTableContents(ChildComp3, 4);
+		Button NextButton = fDialogObjectToolkit.createButton(ChildComp3, "Next Page", new NextButtonSelectionAdapter());
+		Button PrevButton = fDialogObjectToolkit.createButton(ChildComp3, "Previous Page", new PrevButtonSelectionAdapter());
 		return dialogArea;
 	}
 	
 	private void createCombo(Composite parentComposite, int defaultValue)
 	{
-		Combo combo = fDialogObjectToolkit.createCombo(parentComposite, 10, defaultValue);	
+		fDialogObjectToolkit.createCombo(parentComposite, 10, defaultValue);	
 	}
 	
 	private void createLabel(Composite parentComposite, String text) 
@@ -99,31 +110,33 @@ public class CulpritAnalysisDialog extends TitleAreaDialog{
 		fDialogObjectToolkit.createLabel(parentComposite, text);
 	}
 	
-	private void createTableContents(Composite parentComposite, int ColumnNr)
+	private Table createTableContents(Composite parentComposite, int ColumnNr)
 	{
 		Table table = fDialogObjectToolkit.createTable(parentComposite);
-		String[] ColumnNames = {"Tuple", "Occurences", "Fails", "Failure Index"};
-		TableColumn[] column = fDialogObjectToolkit.addColumn(table, 4, ColumnNames);
-		List<TestResultDescription> testResultDescrs = createtestResultDescrs();
+		String[] ColumnNames = {"NumTuple", "Tuple", "Occurences", "Fails", "Failure Index"};
+		TableColumn[] column = fDialogObjectToolkit.addColumn(table, 5, ColumnNames);
 		fillTable(table, testResultDescrs);
 		for (int i = 0; i< column.length; i++)
 		{
 			column[i].pack();
-		}		
+		}
+		return table;
 	}
 	
 	private void fillTable(Table table, List<TestResultDescription> testResultDescrs ) {
 		table.setRedraw(false);
-		TestResultsAnalysis testResultsAnalysis = new TestResultsAnalyzer().generateAnalysis(testResultDescrs, 0, 3);
-		for (int i = 0; i<testResultsAnalysis.getCulpritCount(); i++)
-		{
-			TableItem item = new TableItem(table, SWT.NONE);
-			int c = 0;
-			item.setText(c++, testResultsAnalysis.getCulprit(i).getItem().toString());
-			item.setText(c++, Integer.toString(testResultsAnalysis.getCulprit(i).getOccurenceCount()));
-			item.setText(c++, Integer.toString(testResultsAnalysis.getCulprit(i).getFailureCount()));
-			item.setText(c++, Integer.toString(testResultsAnalysis.getCulprit(i).getFailureIndex()));
-		}
+		
+			for (int i = numRow; i<numRow+27; i++)
+			{
+				TableItem item = new TableItem(table, SWT.NONE);
+				int c = 0;
+				item.setText(c++, Integer.toString(i));
+				item.setText(c++, testResultsAnalysis.getCulprit(i).getItem().toString());
+				item.setText(c++, Integer.toString(testResultsAnalysis.getCulprit(i).getOccurenceCount()));
+				item.setText(c++, Integer.toString(testResultsAnalysis.getCulprit(i).getFailureCount()));
+				item.setText(c++, Integer.toString(testResultsAnalysis.getCulprit(i).getFailureIndex()));
+			}
+		
 		table.setRedraw(true);	
 	}
 	
@@ -146,6 +159,60 @@ public class CulpritAnalysisDialog extends TitleAreaDialog{
 		addTestResult(new String[]{ "7", "7", "3", "9", "8" }, false, testResultDescrs);
 		addTestResult(new String[]{ "2", "4", "5", "3", "8" }, true, testResultDescrs);
 		return testResultDescrs;
+	}
+	class NextButtonSelectionAdapter extends SelectionAdapter {
+		@Override
+		public void widgetSelected(SelectionEvent arg0)
+		{
+			int total = testResultsAnalysis.getCulpritCount();
+			if(total - numRow >= 28)
+			{
+				numRow = numRow + 28;
+				System.out.println("number of rows"+ numRow);
+			}
+			else{
+				numRow = numRow + (total-numRow);
+			}
+			if(numRow < total)
+			{
+			table.removeAll();
+			fillTable(table, testResultDescrs);
+			}
+		}
+		
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e)
+		{
+
+		}
+	
+	}
+	class PrevButtonSelectionAdapter extends SelectionAdapter {
+		@Override
+		public void widgetSelected(SelectionEvent arg0)
+		{
+			int total = testResultsAnalysis.getCulpritCount();
+			if(numRow >= 28)
+			{
+				numRow = numRow - 28;
+				System.out.println("number of rows"+ numRow);
+			}
+			else{
+				System.out.println("number of rows"+ numRow);
+			}
+			if(numRow < total)
+			{
+			table.removeAll();
+			fillTable(table, testResultDescrs);
+			}
+		}
+		
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e)
+		{
+
+		}
+	
 	}
 
 	public static void main(String[] args)
