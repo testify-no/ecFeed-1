5766a77913eba4bcb76419e15521062717cfd49e
hagertestify <hager.debech@testify.no>
2017-09-18 14:18:15 +0200

    removed a comment

--- ecfeed/src/com/ecfeed/ui/modelif/AbstractTestInformer.java
+++ ecfeed/src/com/ecfeed/ui/modelif/AbstractTestInformer.java
@@ -71,7 +71,7 @@ public abstract class AbstractTestInformer {
 		return false;
 	}
 
-	protected void clearFailedTests(){
+	protected void clearFailedTests(){ 
 		fUnsuccesfullExecutionStatuses.clear();
 	}
 
