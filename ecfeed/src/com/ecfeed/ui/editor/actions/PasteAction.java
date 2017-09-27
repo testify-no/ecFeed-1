c521efcaf2b7559aef91ddc8801a167a17d430a6
Marek Kuczabinski <m.kuczabinski@radytek.com>
2017-09-27 08:02:45 +0200

    Unused code removed from PasteAction.

--- ecfeed/src/com/ecfeed/ui/editor/actions/PasteAction.java
+++ ecfeed/src/com/ecfeed/ui/editor/actions/PasteAction.java
@@ -30,50 +30,24 @@ public class PasteAction extends ModelModifyingAction {
 
 	private IJavaProjectProvider fJavaProjectProvider;
 	private TreeViewer fTreeViewer;
-	private int fIndex;
 
 	public PasteAction(
-			ISelectionProvider selectionProvider, 
-			IModelUpdateContext updateContext,
-			IJavaProjectProvider javaProjectProvider) {
-		this(-1, selectionProvider, updateContext, javaProjectProvider);
-	}
-
-	public PasteAction(
-			int index, 
 			ISelectionProvider selectionProvider, 
 			IModelUpdateContext updateContext,
 			IJavaProjectProvider javaProjectProvider) {
 		super(ActionId.PASTE, selectionProvider, updateContext);
-		fIndex = index;
 		fJavaProjectProvider = javaProjectProvider;
-	}
-
-	public PasteAction(
-			TreeViewer treeViewer, 
-			IModelUpdateContext updateContext,
-			IJavaProjectProvider javaProjectProvider) {
-		this(-1, treeViewer, updateContext, javaProjectProvider);
-	}
-
-	public PasteAction(
-			int index, 
-			TreeViewer treeViewer, 
-			IModelUpdateContext updateContext,
-			IJavaProjectProvider javaProjectProvider) {
-		this(index, (ISelectionProvider)treeViewer, updateContext, javaProjectProvider);
-		fTreeViewer = treeViewer;
 	}
 
 	@Override
 	public boolean isEnabled(){
-		
+
 		List<AbstractNode> selectedNodes = getSelectedNodes();
-		
+
 		if (selectedNodes == null) {
 			return false;
 		}
-		
+
 		if (getSelectedNodes().size() != 1) {
 			return false;
 		}
@@ -83,14 +57,12 @@ public class PasteAction extends ModelModifyingAction {
 						getSelectedNodes().get(0), 
 						getUpdateContext(), 
 						fJavaProjectProvider);
-		if (fIndex != -1){
-			return nodeIf.pasteEnabled(NodeClipboard.getContent(), fIndex);
-		}
+
 		return nodeIf.pasteEnabled(NodeClipboard.getContent());
 	}
 
 	@Override
-	public void run(){
+	public void run() {
 		AbstractNode parent = getSelectedNodes().get(0);
 		AbstractNodeInterface parentIf = 
 				NodeInterfaceFactory.getNodeInterface(parent, getUpdateContext(), fJavaProjectProvider);
@@ -104,7 +76,7 @@ public class PasteAction extends ModelModifyingAction {
 		}
 
 		parentIf.addChildren(childrenToAdd);
-		if(fTreeViewer != null){
+		if(fTreeViewer != null) {
 			fTreeViewer.expandToLevel(parent, 1);
 		}
 	}
