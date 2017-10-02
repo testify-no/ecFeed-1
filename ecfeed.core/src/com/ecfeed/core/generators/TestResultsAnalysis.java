f7d8aa0fb04962c3f69105efd91773e80bcce186
hagertestify <hager.debech@testify.no>
2017-10-02 10:26:45 +0200

    removed unused import

--- ecfeed.core/src/com/ecfeed/core/generators/TestResultsAnalysis.java
+++ ecfeed.core/src/com/ecfeed/core/generators/TestResultsAnalysis.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import org.apache.commons.math3.distribution.HypergeometricDistribution;
 
 public class TestResultsAnalysis {
 
