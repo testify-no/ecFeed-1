3f4802cb40073f89699482246b30a1d21fe6b2bb
Marek Kuczabinski <m.kuczabinski@radytek.com>
2017-10-24 12:52:45 +0200

    Comments added.

--- ecfeed.core/src/com/ecfeed/core/adapter/operations/EditorStyleChecker.java
+++ ecfeed.core/src/com/ecfeed/core/adapter/operations/EditorStyleChecker.java
@@ -41,12 +41,14 @@ public class EditorStyleChecker implements IModelVisitor{
 		}
 
 		List<ClassNode> classesInRootNode = rootNode.getClasses();
+		
 		for (ClassNode classNode: classesInRootNode) {
 			errorDescription = (ErrorDescription) classNode.accept(this);
 			if (errorDescription != null) {
 				return errorDescription;
 			}
 		}
+		
 		return null;
 	}
 
@@ -54,6 +56,7 @@ public class EditorStyleChecker implements IModelVisitor{
 	public Object visit(ClassNode node) throws Exception {
 
 		RootNode root = node.getRoot();
+		
 		DuplicateChecker duplicateChecker = new DuplicateChecker(root.getClasses(), null);
 		ErrorDescription errorDescription = duplicateChecker.checkForDuplicateClasses();
 		if (errorDescription != null) {
@@ -74,11 +77,13 @@ public class EditorStyleChecker implements IModelVisitor{
 	public Object visit(MethodNode node) throws Exception {
 		
 		ClassNode parent = node.getClassNode();
+		
 		DuplicateChecker duplicateChecker = new DuplicateChecker(null, parent.getMethods());
 		ErrorDescription errorDescription = duplicateChecker.checkForDuplicateMethods();
 		if (errorDescription != null) {
 			return errorDescription;
-		}		
+		}	
+		
 		return null;
 	}
 
@@ -107,11 +112,17 @@ public class EditorStyleChecker implements IModelVisitor{
 		return null;
 	}
 
-	public class ErrorDescription {
+	// XMQX - static qualifier added
+	public static class ErrorDescription {
 		private AbstractNode fNode;
 		private CheckErrorCode fErrorCode;
 
-		public ErrorDescription(AbstractNode node, CheckErrorCode errorCode) {
+		// XMQX - we have to add another node fDuplicateNode to generate the message so that
+		// duplicate nodes are easy to find
+		public ErrorDescription(
+				AbstractNode node, 
+				// AbstractNode duplicateNode,
+				CheckErrorCode errorCode) {
 			fNode = node;
 			fErrorCode = errorCode;
 		}
@@ -129,7 +140,9 @@ public class EditorStyleChecker implements IModelVisitor{
 		DUPLICATE_CLASS_NAME, DUPLICATE_METHOD_NAME;	
 	}
 
-	public class DuplicateChecker {
+	// XMQX - this class has two responsibilities - divide into two separate classes + maybe plus a helper class
+	// XMQX - static qualifier added
+	public static class DuplicateChecker {
 
 		private List<ClassNode> fClasses;
 		private List<MethodNode> fMethods;
@@ -143,7 +156,16 @@ public class EditorStyleChecker implements IModelVisitor{
 			List<String> classNameList = makeClassNamesList();
 			List<String> duplicatedNames = new ArrayList<String>();
 
-			if (hasDuplicate(classNameList)) {
+			// XMQX - I would to sth. like this
+			
+			// Pair<ClassNode, ClassNode> duplicates = getFirstPairOfDuplicates(fClasses)
+			// if (duplicates != null) {
+			//      return new ErrorDescription(duplicates.getFirst(), duplicates.getSecond(), CheckErrorCode.DUPLICATE_CLASS_NAME);
+		    // }
+			
+			if (hasDuplicate(classNameList)) { // XMQX - this is not necessary, 
+				// it is enough to check if duplicatedNames.isEmpty()
+				
 				duplicatedNames = getDuplicate(classNameList);
 
 				for (ClassNode cl: fClasses) {
@@ -157,10 +179,19 @@ public class EditorStyleChecker implements IModelVisitor{
 
 		public ErrorDescription checkForDuplicateMethods() {
 
+			// XMQX - avoid nested if/for/it etc.
+			// extract if (fMethods.get(i).. to a method e.g. compareTwoMethods(m1,m2);
+			
+			// XMQX -- nearly square complexity of this algorithm can be avoided
+			// using sort + compare neighbours - but Ok this could be because 
+			// there are not so many methods in the class
 			for (int i = 0; i < fMethods.size()-1; i++) {
 				for (int j = i+1; j < fMethods.size(); j++) {
 					if (fMethods.get(i).getName().equals(fMethods.get(j).getName())) {
+						// XMQX - too long line - around 100 is ok
 						if (compareParametersTypes(fMethods.get(i).getParameterTypes(), fMethods.get(j).getParameterTypes())) {
+							
+							// XMQX - this will be: ErrorDescription(fMethods.get(i), fMethods.get(j), CheckErrorCode.DUPLICATE_METHOD_NAME);
 							return new ErrorDescription(fMethods.get(i), CheckErrorCode.DUPLICATE_METHOD_NAME);
 						}
 					}
@@ -169,26 +200,40 @@ public class EditorStyleChecker implements IModelVisitor{
 			return null;
 		}
 
+		// XMQX rename to areParameterTypesEqual - this is an exception which 
+		// refers only to boolean methods - use are... or is... 
 		private boolean compareParametersTypes(List<String> parameterTypes, List<String> parameterTypes2) {
 
+			// XMQX - check obvious cases as early as possible (before creating the list)
+			// if (parameterTypes.size() == parameterTypes2.size()) {
+			//		return false;
+			// }
+			
+			
 			List<String> numericList = Arrays.asList("int", "double", "float", "long", "short", "byte");
 
+			// XMQX - this should generate an exception (also move it to the first place in the method)
 			if (parameterTypes == null && parameterTypes2 == null) {
 				return true;
 			}
 			
 			if (parameterTypes.size() == parameterTypes2.size()) {
 				
+				// MQMX - i think, we should not sort the parameters, just compare if 
+				// the first parameteters match in both methods (eg. the one is int and another float etc.)
+				// then the next and so on
 				Collections.sort(parameterTypes);
 				Collections.sort(parameterTypes2);
 				if (parameterTypes.equals(parameterTypes2)) {
 					return true;
 				}
 
+				// XMQX
 				if (!Collections.disjoint(parameterTypes, numericList) && !Collections.disjoint(parameterTypes2, numericList)) {
 					return true;	
 				}
 			}
+			
 			return false;
 		}
 
@@ -198,13 +243,19 @@ public class EditorStyleChecker implements IModelVisitor{
 			for (ClassNode cl: fClasses) {
 				classNames.add(cl.getName());
 			}
+			
 			return classNames;
 		}
 
+		
+		// XMQX - it is not necessary to get all the duplicates - just the first pair
 		private List<String> getDuplicate(List<String> list) {
 
 			final List<String> duplicatedNames = new ArrayList<String>();
+			
 			Set<String> set = new HashSet<String>(){
+
+				private static final long serialVersionUID = 5714004217588411946L;
 
 				@Override
 				public boolean add(String name){
