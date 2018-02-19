6a70023a776ca28c07bbadaafff185f23a0b115d
Marek Kuczabinski <m.kuczabinski@radytek.com>
2018-02-19 15:33:37 +0100

    Junit before and after. Static runner renamed.

---
+++ ecfeed.core/src/com/ecfeed/junit/StaticRunnerMethodLg.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.junit;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+
+import com.ecfeed.core.adapter.java.ModelClassLoader;
+import com.ecfeed.core.model.TestCaseNode;
+import com.ecfeed.core.utils.EcException;
+
+public class StaticRunnerMethodLg extends AbstractFrameworkMethod {
+
+	private Collection<TestCaseNode> fTestCases;
+
+	public StaticRunnerMethodLg(Method method, Collection<TestCaseNode> testCases, ModelClassLoader loader) {
+		super(method, loader);
+		fTestCases = testCases;
+	}
+
+	@Override
+	public Object invokeExplosively(Object target, Object... parameters) throws Throwable{
+
+		int totalTestCaseCounter = 0;
+		int failedTestCaseCounter = 0;
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (TestCaseNode testCase : fTestCases) {
+
+			totalTestCaseCounter++;
+
+			try {
+				super.invoke(target, testCase.getTestData());
+
+			} catch (Throwable e) {
+
+				if (failedTestCaseCounter == 0) {
+					JavaMethodHelper.appendFailedTestsMessage(stringBuilder);
+				}
+
+				failedTestCaseCounter++;
+				JavaMethodHelper.appendExceptionMessage(
+						testCase.getMethod().getName(),
+						testCase.getTestData(),
+						e, 
+						stringBuilder);
+			}
+		}
+
+		JavaMethodHelper.addTestStatistics(totalTestCaseCounter, failedTestCaseCounter, stringBuilder);
+
+		if (failedTestCaseCounter > 0) {
+			EcException.report(stringBuilder.toString());
+		}
+
+		return null;
+	}
+
+}
