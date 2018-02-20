7569a0276e7c3654ad5b882b83904a6f3eff12c6
Marek Kuczabinski <m.kuczabinski@radytek.com>
2018-02-20 10:14:23 +0100

    Junit before and after. Static runner added with tests.

---
+++ ecfeed/test/com/ecfeed/junit/staticrunner/LauncherForStaticRunnerTests.java
@@ -0,0 +1,43 @@
+package com.ecfeed.junit.staticrunner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+import com.ecfeed.core.utils.StringHelper;
+
+public class LauncherForStaticRunnerTests {
+
+	private static StringBuilder fLog;
+	
+	public static void appendToLog(String message) {
+		fLog.append(message);
+	}
+	
+	@Test
+	public void shouldRunAllTestCases() {
+
+		fLog = new StringBuilder();
+		
+		JUnitCore jUnitCore = new JUnitCore();
+		Result result = jUnitCore.run(StaticRunnerTestAll.class);
+		
+		assertTrue(result.wasSuccessful());
+		assertTrue(StringHelper.isEqual("B F(1,1) A B F(2,1) A", fLog.toString().trim()));
+	}	
+	
+	@Test
+	public void shouldRunSuite2() {
+		
+		fLog = new StringBuilder();
+		
+		JUnitCore jUnitCore = new JUnitCore();
+		Result result = jUnitCore.run(StaticRunnerTestSuites.class);
+		
+		assertTrue(result.wasSuccessful());
+		assertTrue(StringHelper.isEqual("B F(1,2) A B F(2,2) A", fLog.toString().trim()));
+	}	
+}
+
