372c847f0042060ef087880bb0fe046bf73c4ced
Marek Kuczabinski <m.kuczabinski@radytek.com>
2018-02-21 12:09:54 +0100

    Junit before and after. Generation of separate methods. Unit tests added.

--- ecfeed.core/src/com/ecfeed/junit/OnlineRunner.java
+++ ecfeed.core/src/com/ecfeed/junit/OnlineRunner.java
@@ -10,6 +10,8 @@
 
 package com.ecfeed.junit;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.runners.model.FrameworkMethod;
@@ -17,6 +19,8 @@ import org.junit.runners.model.InitializationError;
 
 import com.ecfeed.core.generators.api.IGenerator;
 import com.ecfeed.core.model.ChoiceNode;
+import com.ecfeed.core.model.TestCaseNode;
+import com.ecfeed.core.runner.Messages;
 import com.ecfeed.core.runner.RunnerException;
 
 public class OnlineRunner extends AbstractOnlineRunner {
@@ -27,10 +31,34 @@ public class OnlineRunner extends AbstractOnlineRunner {
 
 	protected void addFrameworkMethods(
 			FrameworkMethod frameworkMethod,
-			IGenerator<ChoiceNode> generator,
+			IGenerator<ChoiceNode> initializedGenerator,
 			List<FrameworkMethod> inOutFrameworkMethods) throws RunnerException {
 
-		inOutFrameworkMethods.add(new CollectiveOnlineRunnerMethod(frameworkMethod.getMethod(), generator, getLoader()));
+		List<ChoiceNode> listOfChoices = new ArrayList<>();
+
+		for (;;) {
+
+			try {
+				listOfChoices = initializedGenerator.next();
+
+			} catch (Exception e){
+				RunnerException.report(Messages.RUNNER_EXCEPTION(e.getMessage()));
+			}
+
+			if (listOfChoices == null) {
+				break;
+			}
+
+			Collection<TestCaseNode> listWithOneTestCase = new ArrayList<TestCaseNode>();
+			listWithOneTestCase.add(new TestCaseNode(new String(), listOfChoices));
+
+			inOutFrameworkMethods.add(
+					new StaticRunnerMethod(
+							frameworkMethod.getMethod(), 
+							listWithOneTestCase, 
+							getLoader()));
+		}
+
 	}
 
 }
