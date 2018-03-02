2a8793b218d90102d90b3c3dd98ca7808409779c
akarasinski <akarasinski@github.com>
2018-03-02 12:13:08 +0100

    Convert to range, value or regex on the fly.

--- ecfeed/src/com/ecfeed/ui/editor/ChoiceDetailsPage.java
+++ ecfeed/src/com/ecfeed/ui/editor/ChoiceDetailsPage.java
@@ -21,6 +21,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 import com.ecfeed.application.ApplicationContext;
@@ -50,6 +51,7 @@ public class ChoiceDetailsPage extends BasicDetailsPage {
 	private Button fExpectedCheckbox;
 	private Button fRandomizeCheckbox;
 	private MethodParameterInterface fParameterIf;
+	private Label fValueLabel;
 
 	public ChoiceDetailsPage(
 			IMainTreeProvider mainTreeProvider,
@@ -144,7 +146,7 @@ public class ChoiceDetailsPage extends BasicDetailsPage {
 		}
 		items.add(fChoiceIf.getValue());
 		fValueCombo.setItems(items.toArray(new String[]{}));
-		setValueComboText(choiceNode);
+		setValueComboText(choiceNode);		
 		fValueCombo.addSelectionListener(new ValueSelectedListener());
 		fValueCombo.addFocusListener(new ValueFocusLostListener());
 
@@ -156,7 +158,24 @@ public class ChoiceDetailsPage extends BasicDetailsPage {
 		fRandomizeCheckbox.setSelection(choiceNode.isRandomizeValue());
 		fRandomizeCheckbox.setEnabled(isRandomizeCheckboxEnabled());
 
+		updateValueLabel(choiceNode);
+
 		fAttributesComposite.layout();
+	}
+
+	private void updateValueLabel(ChoiceNode choiceNode) {
+		String type = fChoiceIf.getParameter().getType();
+		boolean isRandomizedValue = choiceNode.isRandomizeValue();
+
+		if (isRandomizedValue) {
+			if (type.equals(JavaTypeHelper.TYPE_NAME_STRING)) {
+				fValueLabel.setText("Regex");
+			} else {
+				fValueLabel.setText("Range");
+			}
+		} else {
+			fValueLabel.setText("Value");
+		}
 	}
 
 	private boolean isRandomizeCheckboxEnabled() {
@@ -200,7 +219,7 @@ public class ChoiceDetailsPage extends BasicDetailsPage {
 						fAttributesComposite, "Randomize value", new RandomizedApplier());
 		SwtObjectHelper.setHorizontalSpan(fRandomizeCheckbox, 3);
 
-		getEcFormToolkit().createLabel(fAttributesComposite, "Value");
+		fValueLabel = getEcFormToolkit().createLabel(fAttributesComposite, "Value");
 		getEcFormToolkit().paintBordersFor(fAttributesComposite);
 	}
 
@@ -211,7 +230,35 @@ public class ChoiceDetailsPage extends BasicDetailsPage {
 		public void applyValue() {
 			fChoiceIf.setRandomize(fRandomizeCheckbox.getSelection());
 			fRandomizeCheckbox.setSelection(fChoiceIf.isRandomize());
+			updateValueLabel(getSelectedChoice());
+			switchValueOnThefly();
 		}
+	}
+
+	private void switchValueOnThefly() {
+		boolean isRandomized = fChoiceIf.isRandomize();
+		String fValueComboText = fValueCombo.getText();
+		String type = fChoiceIf.getParameter().getType();
+		if (!type.equals(JavaTypeHelper.TYPE_NAME_STRING)) {
+			if (isRandomized) {
+				fValueComboText = convertFromValueToRange(fValueComboText);
+			} else {
+				fValueComboText = convertFromRangeToValue(fValueComboText);
+			}
+			fValueCombo.setText(fValueComboText);
+			setValueComboToModel();
+		}
+		//refresh maybe?
+	}
+
+	public static final String DELIMITER = ":";
+
+	private String convertFromValueToRange(String value) {
+		return value+DELIMITER+value;
+	}
+
+	private String convertFromRangeToValue(String value) {
+		return value.split(DELIMITER)[0];
 	}
 
 	@Override
@@ -242,7 +289,6 @@ public class ChoiceDetailsPage extends BasicDetailsPage {
 		public void focusLost(FocusEvent e) {
 			setValueComboToModel();
 		}
-
 	}
 
 	private void setValueComboToModel() {
