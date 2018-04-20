22ca3265e53e09a059640136deda4803e6c4c4e9
akarasinski <akarasinski@github.com>
2018-04-20 18:50:04 +0200

    Set/parse/change parameter type from gui, tests.

---
+++ ecfeed/test/com/ecfeed/adapter/operations/MethodParameterOperationSetTypeTest.java
@@ -0,0 +1,17 @@
+package com.ecfeed.adapter.operations;
+
+import org.junit.runner.RunWith;
+
+import com.ecfeed.core.generators.CartesianProductGenerator;
+import com.ecfeed.junit.CollectiveOnlineRunner;
+import com.ecfeed.junit.annotations.Constraints;
+import com.ecfeed.junit.annotations.EcModel;
+import com.ecfeed.junit.annotations.Generator;
+
+@RunWith(CollectiveOnlineRunner.class)
+@Generator(CartesianProductGenerator.class)
+@EcModel("test/com/ecfeed/adapter/operations/Inntekstkomponenten.ect")
+@Constraints(Constraints.ALL)
+public class MethodParameterOperationSetTypeTest {
+
+}
