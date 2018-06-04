c683409efa1ba8033ddd1ee38be166c215185100
Marek Kuczabinski <m.kuczabinski@radytek.com>
2018-06-04 11:07:30 +0200

    Displaying ambiguous text on generator dialog.

--- ecfeed/src/com/ecfeed/ui/dialogs/GeneratorSetupDialog.java
+++ ecfeed/src/com/ecfeed/ui/dialogs/GeneratorSetupDialog.java
@@ -270,18 +270,6 @@ public abstract class GeneratorSetupDialog extends TitleAreaDialog {
 		updateOkButtonAndErrorMsg();
 	}
 
-	private Text createAmbiguousWarningText(Composite parent) {
-
-		Text txt = new Text(parent, SWT.BORDER | SWT.V_SCROLL | SWT.READ_ONLY);
-
-		txt.setLayoutData(
-				new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
-
-		txt.setText("                    ");
-
-		return txt;
-	}
-
 	private void checkAndDisplayAmbiguousStateWarning() {
 
 		List<Constraint> constraints = getCheckedConstraints();
@@ -677,6 +665,7 @@ public abstract class GeneratorSetupDialog extends TitleAreaDialog {
 	}
 
 	private void createGeneratorSelectionComposite(Composite container) {
+
 		Composite generatorComposite = new Composite(container, SWT.NONE);
 		generatorComposite.setLayout(new GridLayout(2, false));
 		generatorComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER,
@@ -688,7 +677,8 @@ public abstract class GeneratorSetupDialog extends TitleAreaDialog {
 		generatorLabel.setText("Generator");
 
 		createGeneratorViewer(generatorComposite);
-		fAmbigousWarningText = createAmbiguousWarningText(container);
+
+		fAmbigousWarningText = DialogObjectToolkit.createGridText(container, 60, new String());
 	}
 
 	private void createTestCasesExportComposite(Composite parentComposite) {
