f1acd77ed181797b44ef2d153c66de29761f2f6a
Marek Kuczabinski <m.kuczabinski@radytek.com>
2018-06-20 09:54:11 +0200

    Constraints evaluation.

--- ecfeed.core/src/com/ecfeed/core/model/Constraint.java
+++ ecfeed.core/src/com/ecfeed/core/model/Constraint.java
@@ -31,6 +31,11 @@ public class Constraint implements IConstraint<ChoiceNode> {
 		fConsequence = consequence;
 	}
 
+	private boolean getChocieEvaluate(List<ChoiceNode> values) {//uglyMethodNeedsRefactor() {
+		
+		return true;
+	}
+	
 	@Override
 	public EvaluationResult evaluate(List<ChoiceNode> values) {
 
