6d3d6491f5473c99c874ba12ce577e379e94c807
Marek Kuczabinski <m.kuczabinski@radytek.com>
2018-06-20 09:51:51 +0200

    Value field helper.

---
+++ ecfeed.core/src/com/ecfeed/core/utils/ChoiceNodeHelper.java
@@ -0,0 +1,10 @@
+package com.ecfeed.core.utils;
+
+import com.ecfeed.core.model.ChoiceNode;
+
+public class ChoiceNodeHelper {
+	public static String adaptChoiceValue(ChoiceNode oldChoiceNode, ChoiceNode newChoiceNode) {
+		
+		return null;
+	}
+}
