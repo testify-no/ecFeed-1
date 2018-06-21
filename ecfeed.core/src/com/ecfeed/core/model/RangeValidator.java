cd17b18d0e27177244d941142ca00b3c04c2b54e
Marek Kuczabinski <m.kuczabinski@radytek.com>
2018-06-21 15:04:38 +0200

    Refactoring.

--- ecfeed.core/src/com/ecfeed/core/model/RangeValidator.java
+++ ecfeed.core/src/com/ecfeed/core/model/RangeValidator.java
@@ -1,6 +1,8 @@
 package com.ecfeed.core.model;
 
 import static com.ecfeed.core.model.EStatementRelation.EQUAL;
+import static com.ecfeed.core.model.EStatementRelation.GREATER_EQUAL;
+import static com.ecfeed.core.model.EStatementRelation.LESS_EQUAL;
 import static com.ecfeed.core.model.EStatementRelation.NOT_EQUAL;
 import static com.ecfeed.core.model.EStatementRelation.GREATER_THAN;
 import static com.ecfeed.core.model.EStatementRelation.LESS_THAN;
@@ -9,9 +11,10 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.ecfeed.core.utils.JavaTypeHelper;
 
-public class RangeAmbiguityValidator {
+public class RangeValidator {
 
 	private static final int SINGLE_VALUE = 1;
+	private static final int RANGE_VALUE = 2;	
 
 	public static final boolean isAmbiguous(
 			String leftRange, 
@@ -116,6 +119,75 @@ public class RangeAmbiguityValidator {
 		} 
 
 		if (a || b || c || d) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static boolean isRightRangeInLeftRange(
+			String leftValues, String rightValues, EStatementRelation relation, String substituteType) {
+
+		if(!JavaTypeHelper.isNumericTypeName(substituteType)) {
+			return false;
+		}
+
+		String[] choices = createRangeArray(leftValues);
+		String[] constraints = createRangeArray(rightValues);
+
+		if (relation.equals(EQUAL)) {
+			return validateEqualCondition(choices, constraints, substituteType);
+		}
+
+		return validateOtherThanEqualCondition(choices, constraints, relation, substituteType);
+	}
+
+
+	private static boolean validateEqualCondition(
+			String[] choices, String[] constraints, String substituteType) {
+		
+		String lowerChoice = choices[0]; 
+		String upperChoice = choices[1];
+		
+		String lowerConstraint = constraints[0];  
+		String upperConstraint = constraints[1];
+
+		int choicesLength = choices.length;
+		int constraintsLength = constraints.length;
+		
+		if (choicesLength == RANGE_VALUE && constraintsLength == RANGE_VALUE) {
+			return GREATER_EQUAL.isMatch(substituteType, upperChoice, lowerConstraint)
+					&& LESS_EQUAL.isMatch(substituteType, lowerChoice, upperConstraint);
+		}
+
+		if (choicesLength == RANGE_VALUE && constraintsLength == SINGLE_VALUE) {
+			return GREATER_EQUAL.isMatch(substituteType, lowerChoice, lowerConstraint)
+					&& GREATER_EQUAL.isMatch(substituteType, upperChoice, lowerConstraint);
+		} 
+
+		if (choicesLength == SINGLE_VALUE && constraintsLength == RANGE_VALUE) {
+			return GREATER_EQUAL.isMatch(substituteType, lowerChoice, lowerConstraint)
+					&& LESS_EQUAL.isMatch(substituteType, lowerChoice, upperConstraint);
+		} 
+
+		if (choicesLength == SINGLE_VALUE && constraintsLength == SINGLE_VALUE) {
+			return EQUAL.isMatch(substituteType, lowerChoice, lowerConstraint);
+		}
+
+		return false;
+	}	
+
+	private static boolean validateOtherThanEqualCondition(
+			String[] choices,
+			String[] constraints,
+			EStatementRelation relation,
+			String substituteType) {
+
+		if (relation.isMatch(substituteType, choices[0], constraints[1])) {
+			return true;
+		}
+
+		if (relation.isMatch(substituteType, choices[1], constraints[0])) {
 			return true;
 		}
 
