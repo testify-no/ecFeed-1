3d16f7655f7bc89449fdd1e3f30b74550f54141e
Marek Kuczabinski <m.kuczabinski@radytek.com>
2018-06-21 15:18:12 +0200

    Refactoring.

--- ecfeed.core/src/com/ecfeed/core/model/ConditionHelper.java
+++ ecfeed.core/src/com/ecfeed/core/model/ConditionHelper.java
@@ -1,8 +1,25 @@
 package com.ecfeed.core.model;
 
+import com.ecfeed.core.utils.ExceptionHelper;
+import com.ecfeed.core.utils.JavaTypeHelper;
 import com.ecfeed.core.utils.MessageStack;
 
 public class ConditionHelper {
+
+	public static String getSubstituteType(RelationStatement parentRelationStatement) {
+
+		String substituteType = 
+				JavaTypeHelper.getSubstituteType(
+						parentRelationStatement.getLeftParameter().getType(), 
+						JavaTypeHelper.getStringTypeName());
+
+		if (substituteType == null) {
+			final String MESSAGE = "Substitute type must not be null.";
+			ExceptionHelper.reportRuntimeException(MESSAGE);
+		}
+
+		return substituteType;
+	}
 
 	public static void addValuesMessageToStack(
 			String left, EStatementRelation relation, String right, MessageStack messageStack) {
@@ -25,7 +42,7 @@ public class ConditionHelper {
 	public static String createMessage(String name, String value) {
 		return name + " [" + value + "].";
 	}
-	
+
 	public static String createMessage(String name, String value, String additionalMessage) {
 		return name + " [" + value + "] " + additionalMessage + ".";
 	}	
